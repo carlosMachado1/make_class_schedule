e5e89e022d619889892339d603b9612c92be89d3
Carlos Gabriel Machado <carlos.machado1980@yahoo.com>
2021-01-13 06:54:34 -0400

    Styles working. Starting to read my schedule.

--- class_schedule.py
+++ class_schedule.py
@@ -1,6 +1,17 @@
+# -*- coding: utf-8 -*-
+
 from openpyxl import Workbook
-from openpyxl.styles import Alignment, Font, Border, Side, PatternFill
+
+from openpyxl.styles import (
+    Alignment,
+    Font,
+    Border,
+    Side,
+    PatternFill,
+)
+
 from progression_time import hour_progression
+# from file_reader import read_data
 
 
 wb = Workbook()
@@ -9,7 +20,6 @@ dest_filename = "/home/carlos-machado/Documents/teste.xlsx"
 ws1 = wb.active
 ws1.title = "Schedule"
 
-# days of week
 days = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"]
 columns = "BCDEFGH"
 horarios = hour_progression(1)
@@ -19,30 +29,43 @@ horarios = hour_progression(1)
 #
 
 # head alignment/font [from days of week]
-head_font = Font(name="Times New Roman", size=16, bold=True)
+head_font = Font(name="Times New Roman", size=16, bold=True, color="CECAC5")
 general_alignment = Alignment(horizontal="center", vertical="center")
 
 # time font
-time_font = Font(name="Times New Roman", size=13, bold=True)
+time_font = Font(name="Times New Roman", size=13, bold=True, color="CECAC5")
 
-data_font = Font(name="Times New Roman", size=12)
+# subjects font
+data_font = Font(name="Times New Roman", size=12, color="CECAC5")
 
-border = Border(left=Side(style="thin"))
+# border
+border = Border(
+    left=Side(style="thin"),
+    right=Side(style="thin"),
+    top=Side(style="thin"),
+    bottom=Side(style="thin")
+    )
+
+# foreground color for head/time
+headers_fgc = PatternFill(fill_type="solid", fgColor="2C001E")
+
+# foreground color for data[subjects]
+data_fgc = PatternFill(fill_type="solid", fgColor="AEA79F")
 
 #
 # Styles session finished
 #
 
-# background color
-bgc = PatternFill(fill_type="solid", bgColor="000000")
-
 # define our head values and styles
 ws1.column_dimensions["A"].width = 12.2
+ws1.cell(row=1, column=1).border = border
+ws1.cell(row=1, column=1).fill = headers_fgc
 for col in range(2, 9):
-    # ws1.cell(row=1, column=col, value=days[col - 2])
     head = ws1.cell(row=1, column=col, value=days[col - 2])
     head.font = head_font
     head.alignment = general_alignment
+    head.border = border
+    head.fill = headers_fgc
     ws1.column_dimensions[columns[col - 2]].width = 18.24
 
 # define our time values and styles
@@ -50,6 +73,8 @@ ws1.row_dimensions[1].height = 22.4
 for row_ in range(2, len(horarios) + 2):
     time = ws1.cell(row=row_, column=1, value=horarios[row_ - 2])
     time.font = time_font
+    time.border = border
+    time.fill = headers_fgc
     ws1.row_dimensions[row_].height = 22.4
 
 
--- file_reader.py
+++ file_reader.py
@@ -1,3 +1,15 @@
 #
 # Make a regular expression to look for our subjects
 #
+
+import re
+
+
+def read_data(dest_):
+    # data = {}
+    archive = open(file=dest_, mode="r")
+    linhas = archive.readlines()
+    print(len(linhas))
+
+
+a = read_data("file_example.txt")
